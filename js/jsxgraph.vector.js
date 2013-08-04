5bd971cba816bcb92dc1fbd9752ac2967043bc20
Mike Sheldon <elleo@gnu.org>
2013-08-04 17:35:07 +0100

    Add vector type

---
+++ js/jsxgraph.vector.js
@@ -0,0 +1,31 @@
+JXG.Options.vector = {
+	strokeColor: 'black',
+	strokeWidth: 3,
+	point: {
+		visible: false,
+		name: '',
+		withLabel: false
+	}
+};
+
+JXG.createVector = function (board, parents, attributes) {
+	var attr_vector = JXG.copyAttributes(attributes, board.options, 'vector'),
+		attr_point = JXG.copyAttributes(attributes, board.options, 'vector', 'point'),
+		attach = parents[0],
+		direction = parents[1],
+		len = parents[2],
+		distance = JXG.Math.Geometry.distance(direction, [0, 0]),
+		lbyd = len/distance,
+		
+		end = board.create('point', [function () {
+				return attach.X() + lbyd*direction[0];
+			},
+			function () {
+				return attach.Y() + lbyd*direction[1];
+			}], attr_point),
+		vector = board.create('arrow', [attach, end], attr_vector);
+		
+	return vector;
+};
+
+JXG.JSXGraph.registerElement('vector', JXG.createVector);
