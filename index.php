e91549a4531b298e241c167a08040f21f4c09148
Mike Sheldon <elleo@gnu.org>
2013-08-25 21:38:48 +0100

    Add some real text to the overview page

--- index.php
+++ index.php
@@ -12,10 +12,9 @@ require_once('header.php');
 
 				<div>
 					<h2>Overview</h2>
-				
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque posuere sollicitudin porttitor. Morbi lacus ante, pulvinar a libero eget, dignissim ultricies justo. Donec tincidunt accumsan lectus, ut pharetra ligula porta ac. Ut nec interdum tellus. Curabitur sed imperdiet elit. In vitae libero ultricies, luctus dolor eget, condimentum eros. Sed nec suscipit ipsum. Vivamus tempus ligula scelerisque elit fermentum pharetra.</p>
 
-					<p>Morbi bibendum dolor consequat nisl convallis, viverra rutrum risus commodo. Nulla aliquam mi vel consequat tristique. In feugiat nulla vitae velit auctor sagittis. Cras ut aliquet magna, eu auctor magna. Sed varius ullamcorper magna, nec ullamcorper enim accumsan sed. Morbi aliquet iaculis dui quis ultricies. Suspendisse bibendum ut nibh nec sollicitudin. Vestibulum et cursus turpis. Aliquam venenatis, libero ac blandit semper, elit nibh auctor nibh, nec consectetur tortor augue luctus justo. Sed eget malesuada mi. Sed nec mi turpis. Nullam aliquet dolor ac mi tempus elementum. Praesent tempus dolor ut libero porttitor, quis mollis orci vulputate.</p>
+					<p>Welcome to the Mathematics Playground, a suite of interactive examples designed to give students a more intuitive understanding of a range of mathematical concepts. To get started simply select a topic from the menu on the left.</p>
+				
 				</div>
 
 			</div>
