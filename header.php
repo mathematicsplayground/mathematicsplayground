9d02b1cdda9ae016c5e10e1fc707534db99c0410
Mike Sheldon <mike@mikeasoft.com>
2020-04-16 11:50:59 +0100

    Load MathJax over HTTPS

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 				tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}
 			});
 		</script>
-		<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+		<script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
 		<script type="text/javascript" src="js/jquery.min.js"></script>
 		<script type="text/javascript" src="js/jquery-ui.min.js"></script>
 		<script type="text/javascript" src="js/underscore.min.js"></script>
