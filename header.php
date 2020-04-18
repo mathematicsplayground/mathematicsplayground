c073352f6a31fe0462fbd76f1abc5375d9f1264b
Mike Sheldon <mike@mikeasoft.com>
2020-04-18 16:52:26 +0100

    Use cloudflare CDN as mathjax CDN is deprecated

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 				tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}
 			});
 		</script>
-		<script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
 		<script type="text/javascript" src="js/jquery.min.js"></script>
 		<script type="text/javascript" src="js/jquery-ui.min.js"></script>
 		<script type="text/javascript" src="js/underscore.min.js"></script>
